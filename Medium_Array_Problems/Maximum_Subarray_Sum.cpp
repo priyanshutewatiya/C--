a1b97d6498c52114ab82a7bd3a6068a221e9e02b
Priyanshu Tewatiya <teotiapriyanshu1@gmail.com>
2026-04-10 21:51:57 +0530

    Updated

--- Medium_Array_Problems/Maximum_Subarray_Sum.cpp
+++ Medium_Array_Problems/Maximum_Subarray_Sum.cpp
@@ -1,19 +1,21 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-int main(){
+int main()
+{
     int arr[] = {2, 3, 5, -2, 7, -4};
     int n = sizeof(arr) / sizeof(arr[0]);
 
     /*At each index:
 👉 “Should I extend previous subarray OR start new?”
     */
-    int sum = 0 ,maxSum = INT_MIN;
-    for(int i = 0  ; i < n;i++){
-            sum +=arr[i];
-            maxSum = max(maxSum,sum);
-        if(sum <0){
-            sum =0;
+    int sum = 0, maxSum = INT_MIN;
+    for (int i = 0; i < n; i++){
+        sum += arr[i];
+        maxSum = max(maxSum, sum);
+        if (sum < 0)
+        {
+            sum = 0;
         }
     }
     cout << maxSum << endl;
